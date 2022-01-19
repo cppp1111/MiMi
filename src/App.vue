b6d45457186e052ecff1a9021d5c24d223b3c121
root <496729842@qq.com>
2022-01-19 19:01:03 +0800

    4-2JSONP跨域

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-01-19 16:51:31
+ * @LastEditTime: 2022-01-19 18:54:43
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \MiMi\src\App.vue
+-->
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
@@ -7,11 +15,25 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+// import axios from 'axios'
+import jsonp from 'jsonp'
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data(){
+    return{
+      age:30,
+      data:''
+    }
+  },
+  mounted(){
+    let url = 'https://www.imooc.com/activity/servicetime';
+    jsonp(url,(err,res)=>{
+      let result =res;
+      this.data = result;
+    })
   }
 }
 </script>
