9766094bbf56d851959870942ec516813f793804
root <496729842@qq.com>
2022-01-21 22:34:11 +0800

    Nav-Footer组件开发

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,16 +1,62 @@
 <!--
  * @Author: your name
  * @Date: 2022-01-20 09:17:59
- * @LastEditTime: 2022-01-21 16:19:12
+ * @LastEditTime: 2022-01-21 22:33:08
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \MiMi\src\components\NavFooter.vue
 -->
 <template>
-    <div></div>
+    <div class="footer">
+        <div class="footer-logo">
+            <img src="/imgs/logo-footer.png" alt="">
+            <p>小米商城</p>
+        </div>
+        <div class="footer-link">
+            <a href="https://github.com/" target="_blank">cppp1111主页</a><span>|</span>
+            <a href="https://coding.imooc.com/class/113.html" target="_blank">Vue全栈课程</a><span>|</span>
+            <a href="https://coding.imooc.com/class/236.html" target="_blank">React全家桶课程</a><span>|</span>
+            <a href="https://coding.imooc.com/class/343.html" target="_blank">微信支付专项课程（H5+小程序+Node+Mongo）</a>
+        </div>
+        <div class="copyright">Copyright ©2022 <span class="domain">mi.futurefe.com</span> All Rights Reserved.</div>
+    </div>
 </template>
 <script>
 export default {
     name:'nav-footer'
 }
 </script>
+<style lang="scss" scoped>
+    .footer{
+        height: 234px;
+        border-top: 4px solid #FF6600;
+        background-color: #333333;
+        color: #999999;
+        font-size: 16px;
+        text-align: center;
+        .footer-logo{
+            margin-top: 46px;
+            margin-bottom: 31px;
+            img{
+                width: 53px;
+                height: 36px;
+                margin-bottom: 13px;
+            }
+        }
+        .footer-link{
+            a{
+                color: #999999;
+                display: inline-block;
+            }
+            span{
+                margin: 0 10px;
+            }
+            margin-bottom: 13px;
+        }
+        .copyright{
+            .domain{
+                color: #FF6600;
+            }
+        }
+    }
+</style>
