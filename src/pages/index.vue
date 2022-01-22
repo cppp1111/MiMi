492662cd6ad29e70dc32e5ece496b92ab2132a23
root <496729842@qq.com>
2022-01-22 17:37:49 +0800

    首页轮播菜单实现（2）

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2022-01-20 09:12:41
- * @LastEditTime: 2022-01-22 15:31:53
+ * @LastEditTime: 2022-01-22 17:36:21
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \MiMi\src\pages\index.vue
@@ -14,7 +14,16 @@
                     <ul class="menu-wrap">
                         <li class="menu-item">
                             <a href="javascript:;">手机 电话卡</a>
-                            <div class="children"></div>
+                            <div class="children">
+                                <ul v-for="(item,i) in menuList" :key="i">
+                                    <li v-for="(sub,j) in item" :key="j">
+                                        <a :href="sub?'/#/product/'+ sub.id:''">
+                                            <img :src="sub?sub.img:'imgs/item-box-1.png'" alt="">
+                                            {{sub?sub.name:'小米9'}}
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
                         </li>
                         <li class="menu-item">
                             <a href="javascript:;">电视 盒子</a>
@@ -117,6 +126,28 @@
                         id:'',
                         img:'/imgs/slider/slide-5.jpg'
                     }
+                ],
+                menuList:[
+                    [
+                        {
+                            id:30,
+                            img:'/imgs/item-box-1.png',
+                            name:'小米CC9',
+                        },{
+                            id:31,
+                            img:'/imgs/item-box-2.png',
+                            name:'小米8青春版',
+                        },{
+                            id:32,
+                            img:'/imgs/item-box-3.jpg',
+                            name:'Redmi K20 Pro',
+                        },{
+                            id:33,
+                            img:'/imgs/item-box-4.jpg',
+                            name:'移动4G专区',
+                        }
+                    ],
+                    [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
                 ]
             }
         }
@@ -155,6 +186,42 @@
                         }
                         &:hover{
                             background-color: $colorA;
+                            .children{
+                                display: block;
+                            }
+                        }
+                        .children{
+                            display: none;
+                            width: 962px;
+                            height: 451px;
+                            background-color: $colorG;
+                            position: absolute;
+                            top: 0;
+                            left: 264px;
+                            border: 1px solid $colorH;
+                            ul{
+                                display: flex;
+                                justify-content: space-between;
+                                height: 75px;
+                                li{
+                                    height: 75px;
+                                    line-height: 75px;
+                                    // width: 241px;   等同于下一句
+                                    flex: 1;
+                                    padding-left: 23px;
+                                    z-index: 10;
+                                }
+                                a{
+                                    color: $colorB;
+                                    font-size: 14px;
+                                }
+                                img{
+                                    width: 42px;
+                                    height: 35px;
+                                    vertical-align: middle;
+                                    margin-right: 15px;
+                                }
+                            }
                         }
                     }
                 }
