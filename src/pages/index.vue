182974eabfbc3ef50ec16976de1cd1ccdd8d443c
root <496729842@qq.com>
2022-01-22 15:33:38 +0800

    首页轮播菜单实现（1）

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2022-01-20 09:12:41
- * @LastEditTime: 2022-01-22 14:17:56
+ * @LastEditTime: 2022-01-22 15:31:53
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \MiMi\src\pages\index.vue
@@ -10,6 +10,42 @@
     <div class="index">
         <div class="container">
             <div class="swiper-box">
+                <div class="nav-menu">
+                    <ul class="menu-wrap">
+                        <li class="menu-item">
+                            <a href="javascript:;">手机 电话卡</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">电视 盒子</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">笔记本 平板</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">家电 插线板</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">出行 穿戴</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">智能 路由器</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">电源 配件</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">生活 箱包</a>
+                            <div class="children"></div>
+                        </li>
+                    </ul>
+                </div>
                 <swiper :options="swiperOption">
                     <swiper-slide v-for="(item,index) in slideList" :key="index">
                         <a :href="'/#/product/'+item.id"><img :src="item.img" alt=""></a>
@@ -31,7 +67,7 @@
     import ServiceBar from './../components/ServiceBar.vue'
     import {swiper,swiperSlide} from 'vue-awesome-swiper'
     import 'swiper/dist/css/swiper.css'
-import Swiper from 'vue-awesome-swiper/src/swiper.vue'
+    import Swiper from 'vue-awesome-swiper/src/swiper.vue'
     export default {
         name:'index',
         components:{
@@ -79,7 +115,7 @@ import Swiper from 'vue-awesome-swiper/src/swiper.vue'
                         img:'/imgs/slider/slide-4.jpg'
                     },{
                         id:'',
-                        img:'/imgs/slider/slide5.jpg'
+                        img:'/imgs/slider/slide-5.jpg'
                     }
                 ]
             }
@@ -87,10 +123,47 @@ import Swiper from 'vue-awesome-swiper/src/swiper.vue'
     }
 </script>
 <style lang="scss">
+    @import './../assets/scss/config.scss'; 
+    @import './../assets/scss/mixin.scss';
     .index{
         .swiper-box{
+            .nav-menu{
+                position: absolute;
+                width: 264px;
+                height: 451px;
+                z-index: 9;
+                padding: 26px 0;
+                background-color: #55585a7a;
+                box-sizing: border-box;
+                .menu-wrap{
+                    .menu-item{
+                        height: 50px;
+                        line-height: 50px;
+                        a{
+                            position: relative;
+                            display: block;
+                            font-size: 16px;
+                            color: #ffffff;
+                            padding-left: 30px;
+                            &:after{
+                                position: absolute;
+                                right: 30px;
+                                top: 17.5px;
+                                content: ' ';
+                                @include bgImg(10px,15px,'/imgs/icon-arrow.png')
+                            }
+                        }
+                        &:hover{
+                            background-color: $colorA;
+                        }
+                    }
+                }
+            }
             .swiper-container{
                 height: 451px;
+                .swiper-button-prev{
+                    left: 274px;
+                }
                 img{
                     width: 100%;
                     height: 100%;
