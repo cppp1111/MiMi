3f30daa594c0cf23a7cd3fc8b76b6980ea0190d3
root <496729842@qq.com>
2022-01-23 16:10:34 +0800

    手机商品实现

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2022-01-20 09:12:41
- * @LastEditTime: 2022-01-23 13:32:14
+ * @LastEditTime: 2022-01-23 16:09:46
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \MiMi\src\pages\index.vue
@@ -74,7 +74,31 @@
                 <a href="/#/product/30">
                 <img src="/imgs/banner-1.png" alt=""></a>
             </div>
-            <div class="product-box"></div>
+        </div>
+        <div class="product-box">
+            <div class="container">
+            <h2>手机</h2>
+            <div class="wrapper">
+                <div class="banner-left">
+                    <a href="/#/product/35"><img src="/imgs/mix-alpha.jpg" alt=""></a>
+                </div>
+                <div class="list-box">
+                    <div class="list" v-for="(arr,i) in phoneList" :key="i">
+                        <div class="item" v-for="(item,j) in arr" :key="j">
+                            <span :class="{'new-pro':j%2==0}">新品</span>
+                            <div class="item-img">
+                                <img :src="item.mainImage" alt="">
+                            </div>
+                            <div class="item-info">
+                                <h3>{{item.name}}</h3>
+                                <p>{{item.subtitle}}</p>
+                                <p class="price">{{item.price}}元</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
         </div>
         <service-bar></service-bar>
     </div>
@@ -170,7 +194,23 @@
                         id:47,
                         img:'/imgs/ads/ads-4.jpg'
                     }
-                ]
+                ],
+                phoneList:[]
+            }
+        },
+        mounted(){
+            this.init();
+        },
+        methods:{
+            init(){
+                this.axios.get('/products',{
+                    params:{
+                        categoryId:100012,
+                        pagaSize:8
+                    }
+                }).then((res)=>{
+                    this.phoneList = [res.list.slice(0,4),res.list.slice(4,8)]
+                })
             }
         }
     }
@@ -270,6 +310,88 @@
         }
         .banner{
             margin-bottom: 50px;
+        }
+        .product-box{
+            background-color: $colorJ;
+            padding: 30px 0 50px;
+            h2{
+                font-size: $fontF;
+                height: 21px;
+                line-height: 21px;
+                color: $colorB;
+                margin-bottom: 20px;
+            }
+            .wrapper{
+                display: flex;
+                .banner-left{
+                    margin-right: 16px;
+                    img{
+                        width: 224px;
+                        height: 619px;
+                    }
+                }
+                .list-box{
+                    .list{
+                        @include flex();
+                        width: 986px;
+                        margin-bottom: 14px;
+                        &:last-child{
+                            margin-bottom: 0;
+                        }
+                        .item{
+                            width: 236px;
+                            height: 302px;
+                            background-color: $colorG;
+                            text-align: center;
+                            span{
+                                display: inline-block;
+                                width: 67px;
+                                height: 24px;
+                                font-size: 14px;
+                                line-height: 24px;
+                                color: $colorG;
+                                &.new-pro{
+                                    background-color: #7ecf68;
+                                }
+                                &.kill-pro{
+                                    background-color: #e82626;
+                                }
+                            }
+                            .item-img{
+                                img{
+                                    width: 100%;
+                                    height: 195px;
+                                }
+                            }
+                            .item-info{
+                                h3{
+                                    font-size: $fontJ;
+                                    color: $colorB;
+                                    line-height: $fontJ;
+                                    font-weight: bold;
+                                }
+                                p{
+                                    color: $colorD;
+                                    line-height: 13px;
+                                    margin: 6px auto 13px;
+                                }
+                                .price{
+                                    color: #f20a0a;
+                                    font-size: $fontJ;
+                                    font-weight: bold;
+                                    cursor: pointer;
+                                    &:after{
+                                        content: ' ';
+                                        @include bgImg(22px,22px,'/imgs/icon-cart-hover.png');
+                                        margin-left: 5px;
+                                        vertical-align: middle;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 </style>
