2a2aba668d3c804eed0672e926e7b69ba72a91a4
root <496729842@qq.com>
2022-01-23 13:33:23 +0800

    首页广告位实现

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2022-01-20 09:12:41
- * @LastEditTime: 2022-01-22 17:36:21
+ * @LastEditTime: 2022-01-23 13:32:14
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \MiMi\src\pages\index.vue
@@ -65,8 +65,15 @@
                     <div class="swiper-button-next" slot="button-next"></div>
                 </swiper>
             </div>
-            <div class="ads-box"></div>
-            <div class="banner"></div>
+            <div class="ads-box">
+                <a :href="'/#/product/'+item.id" v-for="(item,index) in adsList" :key="index">
+                    <img :src="item.img" alt="">
+                </a>
+            </div>
+            <div class="banner">
+                <a href="/#/product/30">
+                <img src="/imgs/banner-1.png" alt=""></a>
+            </div>
             <div class="product-box"></div>
         </div>
         <service-bar></service-bar>
@@ -148,6 +155,21 @@
                         }
                     ],
                     [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
+                ],
+                adsList:[
+                    {
+                        id:33,
+                        img:'/imgs/ads/ads-1.png'
+                    },{
+                        id:48,
+                        img:'/imgs/ads/ads-2.jpg'
+                    },{
+                        id:45,
+                        img:'/imgs/ads/ads-3.png'
+                    },{
+                        id:47,
+                        img:'/imgs/ads/ads-4.jpg'
+                    }
                 ]
             }
         }
@@ -236,6 +258,18 @@
                     height: 100%;
                 }
             }
+        }
+        .ads-box{
+            @include flex();
+            margin-top: 14px;
+            margin-bottom: 31px;
+            a{
+                width: 296px;
+                height: 167px;
+            }
+        }
+        .banner{
+            margin-bottom: 50px;
         }
     }
 </style>
