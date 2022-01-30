2e4571722b431f3f6672f2b16a2852358b390d62
root <496729842@qq.com>
2022-01-30 16:45:24 +0800

    视频模块实现

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -1,12 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2022-01-22 13:01:02
- * @LastEditTime: 2022-01-27 15:31:02
+ * @LastEditTime: 2022-01-30 14:25:20
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \MiMi\src\declaration.d.ts
  */
-declare module 'Vue'
+declare module 'Vue';
 // declare module 'react'
-declare module 'vue-awesome-swiper'
-declare module 'vue-cookie'
+declare module 'vue-awesome-swiper';
+declare module 'vue-cookie';
