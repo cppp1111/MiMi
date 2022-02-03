66a93f04587c322640a9729e542beb40eaacedad
root <496729842@qq.com>
2022-02-03 22:10:49 +0800

    Babel介绍

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,22 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-19 16:51:31
+ * @LastEditTime: 2022-02-03 22:08:59
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \MiMi\babel.config.js
+ */
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+    "presets": [
+        '@vue/cli-plugin-babel/preset'
+    ],
+    "plugins": [
+        [
+            "component",
+            {
+                "libraryName": "element-ui",
+                "styleLibraryName": "theme-chalk"
+            }
+        ]
+    ]
 }
