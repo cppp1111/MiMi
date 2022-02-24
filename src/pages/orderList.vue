5779ed7e10691fc76ddf105bffd2bfd0c0d4b540
root <496729842@qq.com>
2022-02-24 15:56:35 +0800

    订单列表分页-分页器

--- src/pages/orderList.vue
+++ src/pages/orderList.vue
@@ -46,6 +46,14 @@
               </div>
             </div>
           </div>
+          <el-pagination
+            class="pagination"
+            background
+            layout="prev,pager,next"
+            :pageSize="pageSize"
+            :total="total"
+            @current-change="handleChange"
+          ></el-pagination>
           <no-data v-if="!loading && list.length == 0"></no-data>
         </div>
       </div>
@@ -56,17 +64,22 @@
   import NoData from './../components/NoData.vue'
   import Loading from './../components/Loading.vue'
   import OrderHeader from './../components/OrderHeader'
+  import { Pagination } from 'element-ui'
   export default{
     name:'order-list',
     components:{
       OrderHeader,
       Loading,
-      NoData
+      NoData,
+      [Pagination.name]:Pagination
     },
     data(){
         return{
             list:[],
             loading:true,
+            pageSize:10,
+            pageNum:1,
+            total:0,
         }
     },
     mounted(){
@@ -74,9 +87,14 @@
     },
     methods:{
         getOrderList(){
-            this.axios.get('/orders').then((res)=>{
+            this.axios.get('/orders',{
+              params:{
+                pageNum:this.pageNum
+              }
+            }).then((res)=>{
                 this.loading = false;
                 this.list= res.list;
+                this.total = res.total;
             }).catch(()=>{
                 this.loading = false;
             })
@@ -99,6 +117,10 @@
                     orderNo
                 }
             })
+        },
+        handleChange(pageNum){
+          this.pageNum = pageNum;
+          this.getOrderList();
         }
     }
   }
