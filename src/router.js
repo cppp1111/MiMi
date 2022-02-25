017dce7c7a3264c4f6a2f1d090348cd27a07f712
root <496729842@qq.com>
2022-02-25 10:17:54 +0800

    路由按需加载方案(2)

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-01-19 22:27:17
- * @LastEditTime: 2022-02-25 09:54:30
+ * @LastEditTime: 2022-02-25 10:10:00
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \MiMi\src\router.js
@@ -13,12 +13,12 @@ import Home from './pages/home'
 import Index from './pages/index'
 // import Product from './pages/product'
 // import Detail from './pages/detail'
-import Cart from './pages/cart'
-import Order from './pages/order'
-import OrderConfirm from './pages/orderConfirm'
-import OrderList from './pages/orderList'
-import OrderPay from './pages/orderPay'
-import AliPay from './pages/alipay'
+// import Cart from './pages/cart'
+// import Order from './pages/order'
+// import OrderConfirm from './pages/orderConfirm'
+// import OrderList from './pages/orderList'
+// import OrderPay from './pages/orderPay'
+// import AliPay from './pages/alipay'
 
 Vue.use(Router);
 
@@ -35,40 +35,49 @@ export default new Router({
         }, {
             path: '/product/:id',
             name: 'product',
-            component: resolve => require(['./pages/index.vue'], resolve),
+            component: () =>
+                import ('./pages/product.vue'),
         }, {
             path: '/detail/:id',
             name: 'detail',
-            component: resolve => require(['./pages/detail.vue'], resolve),
+            component: () =>
+                import ('./pages/detail.vue'),
         }]
     }, {
         path: '/login',
         name: 'login',
-        component: resolve => require(['./pages/login.vue'], resolve),
+        component: () =>
+            import ('./pages/login.vue'),
     }, {
         path: '/cart',
         name: 'cart',
-        component: Cart
+        component: () =>
+            import ('./pages/cart.vue'),
     }, {
         path: '/order',
         name: 'order',
-        component: Order,
+        component: () =>
+            import ('./pages/order.vue'),
         children: [{
             path: 'list',
             name: 'order-list',
-            component: OrderList
+            component: () =>
+                import ('./pages/orderList.vue'),
         }, {
             path: 'confirm',
             name: 'order-confirm',
-            component: OrderConfirm
+            component: () =>
+                import ('./pages/orderConfirm.vue'),
         }, {
             path: 'pay',
             name: 'order-pay',
-            component: OrderPay
+            component: () =>
+                import ('./pages/orderPay.vue'),
         }, {
             path: 'alipay',
             name: 'alipay',
-            component: AliPay
+            component: () =>
+                import ('./pages/alipay.vue'),
         }]
     }]
 });
