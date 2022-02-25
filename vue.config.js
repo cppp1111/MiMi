7893835d392a386000e0b81bd409c1623798ec80
root <496729842@qq.com>
2022-02-25 10:47:12 +0800

    删除预加载-真正按需

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-01-19 19:11:42
- * @LastEditTime: 2022-02-24 21:11:07
+ * @LastEditTime: 2022-02-25 10:37:19
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \MiMi\vue.config.js
@@ -25,5 +25,8 @@ module.exports = {
     // indexPath: 'cppp1111.html',
     // lintOnSave:false,
     // 保护main.js
-    productionSourceMap: true
+    productionSourceMap: true,
+    chainWebpack: (config) => {
+        config.plugins.delete('prefetch');
+    }
 }
